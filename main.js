083a0f9dcdb217fafc95554326399c7e33da0bd7
filveith <fil.veith@gmx.com>
2022-03-22 13:59:13 +0100

    changement de terrain bug

--- main.js
+++ main.js
@@ -28,17 +28,20 @@ let boardSize = [9, 9];
     sleep(1000)
 
     await page.click("#qc-cmp2-ui > div.qc-cmp2-footer.qc-cmp2-footer-overlay.qc-cmp2-footer-scrolled > div > button.css-k8o10q")
-    sleep(5000)
+    sleep(2000)
         // await page.click('#tile32')
 
     let myBoard = []
+    let click = [
+        [],
+        []
+    ]
 
     let board = await page.$$('#board img')
     let i = 0
     let rowNb = 0
     let row = []
     boardSize = await get_board_type(page)
-    console.log(boardSize[0]);
 
     while (true) {
         // Get the urls of all the images
@@ -55,18 +58,24 @@ let boardSize = [9, 9];
             }
         }
 
+        boardSize = await get_board_type(page)
 
         let tiles = get_tiles(myBoard)
 
-        let click = check_tiles(myBoard, tiles)
+        click = check_tiles(myBoard, tiles)
             // console.log("left : ", click[0]);
             // console.log("right : ", click[1]);
 
         let left_clicks = click[0]
-        let richt_clicks = click[1]
+        let right_clicks = click[1]
 
-        await mouse_click(right_clicks, 'right')
-        await mouse_click(left_clicks, 'left')
+        if (left_clicks != []) {
+            await mouse_click(page, left_clicks, 'left')
+        }
+        if (right_clicks != []) {
+            await mouse_click(page, right_clicks, 'right')
+
+        }
 
         // /* The above code is clicking on the tiles in the richt_clicks array. */
         // for (let i = 0; i < richt_clicks.length; i++) {
@@ -89,9 +98,9 @@ let boardSize = [9, 9];
         //     }
         // }
 
-        // myBoard.forEach(row => {
-        //     console.log(JSON.stringify(row));
-        // });
+        myBoard.forEach(row => {
+            console.log(JSON.stringify(row));
+        });
 
         // console.log("----------------------------------------------------");
         myBoard = []
@@ -108,14 +117,14 @@ let boardSize = [9, 9];
     // browser.close();
 })()
 
-async function mouse_click(clicks, button) {
+async function mouse_click(page, clicks, mouse_button) {
     /* The above code is clicking on the tiles in the left_clicks array. */
     for (let i = 0; i < clicks.length; i++) {
         try {
-            let tileNb = left_clicks[i][0] * boardSize[0] + left_clicks[i][1]
-            await page.click('#tile' + tileNb, { button: button })
+            let tileNb = clicks[i][0] * boardSize[0] + clicks[i][1]
+            await page.click('#tile' + tileNb, { button: mouse_button })
         } catch (error) {
-            console.log("Error on mouse click");
+            console.log("Error on mouse click :", error);
         }
     }
 }
@@ -151,18 +160,23 @@ function get_tile_type(url) {
         case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAACVBMVEW9vb0Ae3t7e3tXnVpnAAAAKklEQVQI12NYBQQMDQxAACFCQxkYGsFEAAOMgIo5ALmsEALMBSmGaEMQAOO9EHd34ZsRAAAAAElFTkSuQmCC":
             return "6"
         case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAAWlBMVEW9vb17e3v///8AAAAyMjIeHh4GBgaysrKWlpaMjIwZGRm6urq3t7erq6ujo6Obm5t+fn5xcXFra2teXl5OTk5CQkI8PDwRERELCwu0tLSQkJCPj48lJSUkJCTnYh1HAAAAiUlEQVQoz7XRyxKCMBBE0R4D4aGCovj2/3/TLiKOxpmd3mxSOVW9CcQLkIUdBOLJRDCSn1DTO7Tchbg1aV0ENp6/qTmGqbLPiGNzHFVKY9pYKaUxra1fVKaX02aI6VZkdOuAurVovwKrDjlxDM+G+En3DnMcfSeOaRxVuiDrCvnPV0LswOORK/IAq5MFjXfo9T0AAAAASUVORK5CYII=":
+        case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAAVFBMVEW9vb17e3sAAAAyMjIfHx8ZGRkGBgaysrKWlpaMjIy6urq3t7erq6ujo6Obm5uQkJB+fn5xcXFra2teXl5CQkI8PDwkJCQRERELCwu0tLRPT09NTU3ziK3aAAAAeklEQVQoz8XRxw6AIBBF0RFFsff+///pKMgQylZvWBBO8jZAFAqIXMPj70fKmwAVI0t7L1Uxw6bdpXxlT7whesdUOEqkxqgpI5JjFBeaEvmydUMqb7FFSw0guI+OErBstgnHQIWjJt1jOsFNOkuSe5SoBav2619GC3QBU+cEJodccrEAAAAASUVORK5CYII=":
             return [9, 9]
         case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAAdVBMVEW9vb17e3v///8AAAAPDw8TExOJiYlpaWkWFha6urqvr692dnYHBwefn59ubm45OTm3t7e0tLSrq6unp6ednZ2bm5uSkpKDg4NYWFhFRUUrKyshISEdHR2goKCUlJSQkJBbW1taWlpNTU1KSko9PT0yMjIwMDD6GaHeAAAApUlEQVQoz3XSNxIDIRBE0UHNAuu9X3lz/yOKohRphk8VyUs6GFKxiNRJjhSpmAQiIfVHlY1Q9gLqTKKLg89NnCx+WUYJkFwnB2iB6syYrBbI2fQOLKlNGJX9Bt/Wl4xmjZCepRlFnhfyjH0oh/Mu0bPJiyJvHpxMu8K3tobReCB0jIwcgOXmv0SYoQ2R0dKMT0e+7s2pSimUVozEAyAlR/7FKCrqCy/SB4ur07n7AAAAAElFTkSuQmCC":
+        case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAAclBMVEW9vb17e3sAAAAPDw8TExO5ubmvr6+fn5+JiYl2dnZZWVmbm5tubm5oaGgWFhYICAi3t7e0tLSrq6unp6eSkpKIiIiDg4NqampLS0tFRUU8PDw5OTkrKyshISEdHR0YGBigoKCUlJSQkJAyMjIwMDAHBwfyJwQeAAAAnUlEQVQoz63SNw7DMBBEUXuYRCrnLDne/4qmQHe7quxpWDyA+MVerme7BOLtYHZ/oaQ/IT0Bk+bovsMvayil+C4lJAH5aDJAMDTrqNUzQ3uqHOBULwmZeoHfUhtCnQgVouMycmtzPmM1Jr6tHA2Fzd+2GChF5Qi/sYwIxVvI2GJCGQDnAEgmQ7T+W8FlvKrjqZ6UEhVelfx2G+eH+AFR5AaB09ThtAAAAABJRU5ErkJggg==":
             return [16, 16]
         case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAAZlBMVEW9vb17e3sAAAD///+6urq0tLRvb28cHBwODg6rq6ukpKRzc3Nra2tgYGBWVlYiIiIWFha3t7e2traxsbGnp6eKiop/f38XFxcUFBQICAigoKCRkZF5eXlubm5jY2NKSko3NzcoKCh7SYwzAAAAnklEQVQoz4XS6Q6DIBAE4N0tKl6I9Wjt3fd/yYamAk4knb9fJhnIEqdCxIf9EAdC+RLthIFuTYqOZ50lKBdpgXxJRGdAvuRqQKqacqMdLeY+VSqQEcjT01hs5VR5orKLpRjjGcM7knK7MPM9W+L410odASm70qKABvG5AF3d5prdK2agXHTTE6naSgs0P34f2zc1EOQ/UeraOAhSUvgDVpgGnnD0dCwAAAAASUVORK5CYII=":
+        case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAAY1BMVEW9vb17e3sAAAC6urpvb28VFRW0tLQODg6pqamkpKRzc3MiIiIbGxu3t7e2traxsbGsrKyKiop/f39sbGxhYWFbW1tUVFQXFxcICAigoKCRkZFqampjY2NKSko3NzcoKCgfHx9jPwO+AAAAlUlEQVQoz63S2Q6EIAwFUCiboI7b6Di7//+VYqIUGnnzhoSHk0saUsZzYTFR8+c8lxAfcnRTRmdIAtxTwhKA0SlhKdSQhO3kZDaaJ9lZgTQCyTdQsaTysPhgW8ZSFfEYzZ8IEtOhp1o6/O+gkhES6qBZEGog5Enotc3seOWvnpAEM9T+XadgJNR/9o+t3w7p2t3IL+IKQAMFDjaI1OsAAAAASUVORK5CYII=":
             return [30, 16]
         case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAAyVBMVEV7e3u9vb3//\//tHCT/8gDrHSXuJiLrISnuKSL/8AC+t7jjOkHuIiPwQB7xRh3xShz1fhP4og29u7vFoqPQen30bhb3kxD5qgz7wAj92wTBr6/GmpzJkZTLio7XZ2vXY2jZXGDeTFLhQkjvMCDvOB/wPx7yVhr0dBX2ghP2hxL8zQb81AX80QX+4AP+6QLDqarHnJ3NhIfOg4bSc3bYXWLbVFnlNTzoKjLoJi3tICPvOR/wQx3zXxnzXhn1eRT4nA76tAv7yAf+6gEypmiUAAAA7klEQVQoz53PR3aDQBREUb6ru8lJIirnLDnn7P0vykgGAweY6E3vpEpqjCTpoj5KtFkSopqkfxqB541bBbo11jZOzVYABjn1uetACTb7XSB8wMFNRl3ZcYGIHXMBbFdyNyV9plpAfKLYNM1YnU9S6vFPBZh+BG749f2TeMivUqKhIZBl+Iet4lFGmo5CS+u+ML4tv7/Np+L5zx60ApEndkxlTN1HC8htKhKNXy3YUYI+rqlMnUcAirN8wR2VKKnHLVsRtqFrFaKh2DC2frqkKmm6eQh5n6p0fLAQA6ol8jBpNVBr1KEK5ZWJpKbOkV9Z7RFPbhTK+AAAAABJRU5ErkJggg==":
+        case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAAvVBMVEW9vb17e3vtHCT/8gDrHSTrHifuJiLuKSLwQB71fhO+t7jqIyrxRh3xShy9u7vFoqPZXGHjPUPuIyPvOB/0bhb2hRL3kxD4ow35qgz80QX91gTBr6/GmpzJkZTLio7OhIfQen7XZ2vXY2jeTFLjOT/tISPvMCDwPx7zXhn0dBX4ng77vwn8ywb93AT+4AP+6QLDqarHnJ3QeXzSc3bbVFngRUvlNTzoKjLuJSPyUxvyWRr1eRT6tAv7wQj+6gF5k4xXAAAA6ElEQVQoz6WSxXbDMBBF02ehme1gAw1Tmdv//6zKPnbjgFa5Ryvdxcy8mdaNjpZSWqfeZa5UPkgFpXTiNtTTNHFQEiUARgc1IJkETze7dspiQOK2Vj1LZsCPUbAC0JG0V6kgMmz1UaqtEGJrzINK9cmKA7OvNMu/N7/K5/ShrjUO31ETxvsOH/63Yd6hwdJ+bjTvWYvFfMamKHk1myMPWdso2K0/YHnHQU2EDWetZKyGOlbdNwBcLiP4Z/H2ie1w5oSBeZ78uCiXWPenyZcTiH1OBhf35dFPNtKs8hEvrka5fvf629Af4h+V1g9Ka6YK4QAAAABJRU5ErkJggg==":
             return [50, 50]
         case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAAeFBMVEW9vb17e3sAAAD///8dHR1JSUkxMTEODg4FBQWysrKvr6+YmJh3d3dTU1MsLCwiIiK4uLi2traqqqqioqKBgYFycnJkZGRZWVlYWFgmJiYYGBgVFRUSEhIKCgq5ubmkpKScnJyUlJSNjY2GhoZcXFxEREQ8PDw5OTkbfTg2AAAAqElEQVQoz3XNVxKDMAxF0WfFBlND76SX/e8wBFIsyvWHR3NmJIitALFbD4IRk5GwkjDpGEitpfKWlGuaquaU0a8bp6ilfydGGRkFjJRJNaO9STajnswY0Tb1Zfjdpgq+0LrCG+8pB75l0jg6gz2Gr8kZHWQJnIlSuFYXM8KFQpFI6RUd+eCEgD49MaconKRxFwS3fkubYkmIK1vfE3AyYwSxHoa3RUxYL8nOB//PC+1IAAAAAElFTkSuQmCC":
             return [100, 100]
         case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAaCAMAAACelLz8AAAAilBMVEW9vb17e3v///8AAAC6urq3t7e0tLRqampEREQvLy8NDQ0FBQWysrJ/f39SUlJOTk4WFhYUFBSpqamioqKenp6bm5t2dnYqKioPDw+5ubmmpqaNjY2IiIiEhIRhYWFKSkpAQEA9PT02NjY0NDQoKCgjIyMdHR0JCQmrq6uWlpZlZWVZWVlISEgcHBxT10T8AAAA50lEQVQoz43SV47DMAwE0NGO3HtZ2+k92+9/vSX9IdhIDGT0RTwQICjCLAUwb88DA7MkI+FJzIuUHuNzA9gH8nY9WUVri/VhPyPvUJLf2uNXt3RGXyK5CnbcABtHqyAir0kgAsOh3Q6OQpI/H2QAvMfkkOSOsoRXbBNyr9IfA+vImiSDzUhK69mbD1/rBB0lF5EZqaCNhYqxmpBKU5FqVitHFn4XiES52O8KjlRupEiKgmQ47UJXthHLFLCh2MmbUNbA7//GPZ/E8glB91CM83gX3j9nVN9DXwXww/qFX8bStclbokUx/82BC2Sx6WA7AAAAAElFTkSuQmCC":
             return [30, 16]
-
+        case "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAAD1BMVEW9vb0AAAD/AAB7e3v///9UaJa4AAAAS0lEQVQI11WNwQnAMAwDRSawPIEjOkCg++9Wq4SG6nX4sIR7Bws7hnhBhlFYo8iQlcgsQ15UGKichhYtoy+Gn8KYFM67CxHQmfjWH/GqCYCzIeZyAAAAAElFTkSuQmCC":
+            return "N"
         default:
             console.log("ALED CASE INCONUS");
             console.log(url);
